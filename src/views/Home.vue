45c8705f491e48ebe0bb72f019c4b10ffb22c834
Aiden Isaia <aidenisaia@gmail.com>
2021-07-18 09:44:21 -0700

    create function for products and other changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable prettier/prettier */
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <hr />
+    <p>
+      Name:
+      <input type="text" v-model="newproductparams.name" />
+    </p>
+    <p>
+      Description:
+      <input type="text" v-model="newproductparams.description" />
+    </p>
+    <p>
+      Price:
+      <input type="text" v-model="newproductparams.price" />
+    </p>
+    <p>
+      Image:
+      <input type="text" v-model="newproductparams.image_url" />
+    </p>
+    <button v-on:click="createProducts()">Add new recipe</button>
     <div v-for="product in products" v-bind:key="product.id">
       <p>Name: {{ product.name }}</p>
       <p>Description: {{ product.description }}</p>
       <p>Price: {{ product.price }}</p>
+      <img v-bind:src="product.image_url" />
       <hr />
     </div>
   </div>
 </template>
-<style></style>
+<style>
+img {
+  width: 250px;
+}
+</style>
 <script>
 import axios from "axios";
 export default {
@@ -20,6 +40,7 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       products: [],
+      newproductparams: {},
     };
   },
   created: function () {
@@ -27,9 +48,21 @@ export default {
   },
   methods: {
     indexProducts: function () {
-      console.log("In indexProducts");
       axios.get("http://localhost:3000/products").then((response) => {
         this.products = response.data;
+      });
+    },
+    createProducts: function () {
+      var productparams = {
+        name: this.newproductparams.name,
+        price: this.newproductparams.price,
+        description: this.newproductparams.description,
+        image_url: this.newproductparams.image_url,
+      };
+      axios.post("http://localhost:3000/products", productparams).then((response) => {
+        console.log(response.data);
+        this.products.push(response.data);
+        this.newproductparams = {};
       });
     },
   },
